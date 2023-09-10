8303c3e3dcad114bb75558746cdeb1e356ac4aef
Jonathan Ariel Plodzien <52645615+JonyPlo@users.noreply.github.com>
2023-09-10 11:25:51 -0300

    fix: :bug: bug in getEnvironments file
    fix bug in getEnvironments file that threw an error deploying in vercel

--- src/helpers/getEnvironments.js
+++ src/helpers/getEnvironments.js
@@ -1,7 +1,7 @@
 export const getEnvironments = () => {
-  import.meta.env; // Cargo todas las variables de entorno, si no se escribe esta linea entonces solo se retornara un objeto vacio
+  const enviroments = import.meta.env; // Cargo todas las variables de entorno, si no se escribe esta linea entonces solo se retornara un objeto vacio
 
   return {
-    ...import.meta.env, // Esparzo todas las variables de entorno y las retorno
+    ...enviroments, // Esparzo todas las variables de entorno y las retorno
   };
 };
